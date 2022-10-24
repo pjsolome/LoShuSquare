93bb54d75173e49f28f5e576704a2bb492ae062e
Parker Solome <pjsolome@cpp.edu>
2022-10-23 19:27:40 -0700

    Initial commit

---
+++ loShu.c
@@ -0,0 +1,27 @@
+//Function that checks for lo shu square
+
+//Calculate rows
+
+//Calculate columns
+
+//Calculate Diagonals
+
+//Compare sums
+
+//Print result
+
+//Main function
+
+//Test array that is Lo Shu Square
+
+//Test array that is NOT Lo Shu Square
+
+//Loop to find random Lo Shu Square
+
+//Initialize random array
+
+//Check if Lo Shu Square
+
+//Print how many squares it took to find Lo Shu Square
+
+//Print successful Lo Shu Square
